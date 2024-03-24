c593586f0c5663c7d25150ae3953a0fec111113e
DhivinX <d.szaradowski@gmail.com>
2024-03-24 13:12:25 +0000

    feat(vue): add main script and style

---
+++ apps/vue/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <input type="checkbox" id="explorer-toggle" />
+
+  <label
+    for="explorer-toggle"
+    class="xeokit-i18n explorer-toggle-label xeokit-btn fas fa-2x fa-sitemap"
+    data-xeokit-i18ntip="toolbar.toggleExplorer"
+    data-tippy-content="Toggle explorer"
+  ></label>
+
+  <input type="checkbox" id="inspector-toggle" />
+
+  <label
+    id="inspector-toggle-label"
+    for="inspector-toggle"
+    class="xeokit-i18n inspector-toggle-label xeokit-btn fas fa-info-circle fa-2x"
+    data-xeokit-i18ntip="toolbar.toggleProperties"
+    data-tippy-content="Toggle properties"
+  ></label>
+
+  <div id="explorer"></div>
+
+  <div id="toolbar"></div>
+
+  <div id="inspector"></div>
+
+  <select id="project-select"></select>
+
+  <div id="viewer">
+    <canvas id="canvas"></canvas>
+    <canvas id="nav-cube-canvas"></canvas>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
