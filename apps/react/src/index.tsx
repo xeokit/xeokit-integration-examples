70a60d83f22a967f67b8c3afa9badac123df712a
DhivinX <d.szaradowski@gmail.com>
2024-04-05 14:41:00 +0200

    feat(react): add tippy.js library and update dependencies

--- apps/react/src/index.tsx
+++ apps/react/src/index.tsx
@@ -1,15 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Viewer from './Viewer';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <Viewer />
-  </React.StrictMode>,
-);
+root.render(<Viewer />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
